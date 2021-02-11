51d183b53e7380ab7d9ffbe1532829e978d2e7c0
Serhii_Riazanov@epam.com <Serhii_Riazanov@epam.com>
2021-02-11 18:07:40 +0200

    fix binary

--- bin/graphql-to-string.js
+++ bin/graphql-to-string.js
@@ -1,61 +1,15 @@
-var fs = require('fs');
-var os = require('os');
+#!/usr/bin/env node
 
 const from = process.argv[2];
 const to = process.argv[3];
 
-function addFragmentsToQuery(content, dirname) {
-    const matches = [...content.matchAll(/\.{3}/g)];
-    const usedFragments = matches.map(el => {
-        let start = el.index;
-        let end = content.indexOf(os.EOL, start);
-        return content.substring(start + 3, end);
-    });
-    return Array.from((new Set(usedFragments)).values())
-        .filter(name => {
-            return fs.existsSync(`${dirname}fragments/${name}.gql`);
-        }).reduce((acc, name) => {
-            acc += ` ${fs.readFileSync(`${dirname}fragments/${name}.gql`, 'utf-8').replace(/\n|\r/g, ' ')}`;
-            return acc;
-        }, '');
-}
+const parseQueries = require('../index');
 
-function parseQueries(dirname) {
-    let baseTempalte = `module.exports = {
-        addVariables: function (name, variables) {
-            if (!this[name]) throw new Error('empty query');
-            if (!variables) return this[name];
-            return this[name].replace('"valiables":{}', '"variables":' + JSON.stringify(variables));
-        },${os.EOL}`;
-    fs.readdir(dirname, (err, filenames) => {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
-        filenames
-            .filter(name => name.indexOf('.gql') !== -1)
-            .forEach((filename, index, queries) => {
-                let content = fs.readFileSync(dirname + filename, 'utf-8');
-                let contentReplaced = content.replace(/\n|\r/g, ' ');
-                if (content.indexOf('...') !== -1) {
-                    contentReplaced += addFragmentsToQuery(content, dirname);
-                }
-                baseTempalte += `${filename.replace('.gql', '')}:'{"query":"${contentReplaced}","valiables":{}}'`;
-                if (queries.length === (index + 1)) {
-                    baseTempalte += `${os.EOL}};${os.EOL}`;
-                    fs.writeFile(to, baseTempalte, 'utf8', function (err) {
-                        if (err) {
-                            console.log(err);
-                            process.exit(1);
-                        }
-                        console.log(`JS file ${to} has been created`);
-                        process.exit(0);
-                    });
-                } else {
-                    baseTempalte += `,${os.EOL}`;
-                }
-            });
-    });
+try {
+    parseQueries(from, to);
+    console.log(`JS file ${to} has been created`);
+    process.exit(0);
+} catch (err) {
+    console.log(err);
+    process.exit(1);
 }
-
-parseQueries(from);
